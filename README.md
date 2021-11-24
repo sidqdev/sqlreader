f7ebc5a2e8bed4cc388a29a58fc3ecb388f902b2
sidq-corp <dimax258223@gmail.com>
2021-11-24 22:11:04 +0200

    add SQLReader class and docs

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
+<h1>Simple sql reader</h1>
+
+<code>
+<p>To read file</p>
 import sqlreader as sql
 
 
-sql.set_base_dir('sql_queries/')
+sql.set_base_dir('sql_queries/') # default '/'
 q = sql.read('query_name')
 
 OR 
@@ -11,4 +15,13 @@ q = sql.read('sql_queries/query_name')
 
 TO CLEAR CACHE:
     sql.clear_cache()
-    
+</code>
+
+<hr>
+
+<code>
+<p>To creat class SQLReader</p>
+import sqlreader
+sql = sqlreader.SQLReader() # params 'directory_name' and 'auto_clear'
+q = sql.read('query_name')
+</code>
--- sqlreader.py
+++ sqlreader.py
@@ -9,7 +9,7 @@ def set_base_dir(directory_name: str):
     BASE_DIR = directory_name
 
 
-def read(query_name: str, file_extension: str = 'sql'):
+def read(query_name: str, file_extension: str = 'sql') -> str:
     file_name = query_name + '.' + file_extension
     if file_name in CACHED_QUERIES:
         return CACHED_QUERIES[file_name]
@@ -26,5 +26,39 @@ def read(query_name: str, file_extension: str = 'sql'):
 def clear_cache():
     global CACHED_QUERIES
     CACHED_QUERIES = dict()
+
+
+
+class SQLReader:
+    def __init__(self, directory_name: str = '', auto_clear: bool = False) -> None:
+        """
+        directory_name: directory with sql files
+        auto_clear: don`t save queries in cache
+        """
+        if not directory_name.endswith('/'):
+            raise Exception('Directory name must end with "/"')
+        self.base_dir = directory_name
+        self.cached_queries = dict()
+        self.auto_clear = auto_clear
+    
+    def clear_cache(self):
+        """Clear queries cache"""
+        self.cached_queries = dict()
+
+    def read(self, query_name: str, file_extension: str = 'sql') -> str:
+        file_name = query_name + '.' + file_extension
+        if file_name in CACHED_QUERIES:
+            return CACHED_QUERIES[file_name]
+
+        file = open(self.base_dir + file_name, 'r')
+        query = file.read()
+        file.close()
+
+        if not self.auto_clear:
+            CACHED_QUERIES[file_name] = query
+
+        return query
+
+
 
 
